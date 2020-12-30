a9cf7c78adc54a65ede06d112d830da9c875e781
erurang <erurang@naver.com>
2020-12-31 03:13:04 +0900

    adsfdf

--- init.js
+++ init.js
@@ -11,5 +11,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 app.listen(process.env.PORT, () =>
-  console.log(`Listening on http://localhost:${process.env.PORT}`)
+  console.log(`Listening on http://localhost:${process.env.PORT || 8080}`)
 );
